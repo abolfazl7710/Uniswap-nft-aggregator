7a2b7e3ae0d79c0fec11de07277de216ddd7de83
abolfazl7710 <121865579+abolfazl7710@users.noreply.github.com>
2023-01-21 15:44:31 +0330

    Update 4_Genie airdrop.py

--- pages/4_Genie airdrop.py
+++ pages/4_Genie airdrop.py
@@ -11,6 +11,7 @@ st.set_page_config(
 st.title("💠 Genie airdrop")
 st.sidebar.success("💠 Genie airdrop")
 
+@st.cache(ttl=10000)
 def querying_pagination(query_string):
     sdk = ShroomDK('8c37dc3a-fcf4-42a1-a860-337fa9931a2a')
     result_list = []
