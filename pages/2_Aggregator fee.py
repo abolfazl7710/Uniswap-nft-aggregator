aad1985b018ec2883d9555fa5dfaf57b981493ce
abolfazl7710 <121865579+abolfazl7710@users.noreply.github.com>
2023-01-21 15:43:46 +0330

    Update 2_Aggregator fee.py

--- pages/2_Aggregator fee.py
+++ pages/2_Aggregator fee.py
@@ -11,6 +11,7 @@ st.set_page_config(
 st.title("💲 Aggregator fee")
 st.sidebar.success("💲 Aggregator fee")
 
+@st.cache(ttl=10000)
 def querying_pagination(query_string):
     sdk = ShroomDK('8c37dc3a-fcf4-42a1-a860-337fa9931a2a')
     result_list = []
