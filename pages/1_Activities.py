e91656c9254c4295dbfbbc188a9b6054ac191812
abolfazl7710 <121865579+abolfazl7710@users.noreply.github.com>
2023-01-21 15:42:46 +0330

    Update 1_Activities.py

--- pages/1_Activities.py
+++ pages/1_Activities.py
@@ -11,6 +11,7 @@ st.set_page_config(
 st.title("📈 Aggregator activities")
 st.sidebar.success("📈 Aggregator activities")
 
+@st.cache(ttl=10000)
 def querying_pagination(query_string):
     sdk = ShroomDK('8c37dc3a-fcf4-42a1-a860-337fa9931a2a')
     result_list = []
